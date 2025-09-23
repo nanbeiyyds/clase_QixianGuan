b7c694b370f8b36edf92057bd4634bdece555611
Qixian Guan <151907247+nanbeiyyds@users.noreply.github.com>
2025-09-23 20:12:03 +0200

    Update ej2b.php

--- ejercicios/unidad2/ej2b.php
+++ ejercicios/unidad2/ej2b.php
@@ -1,1 +1,27 @@
+<html>
+    <head>
+        <title>EJ2B – Conversor Decimal a base n </title>
+    </head>
+    <body>
+        <?php
+            $num = 48;
+            $base = 8;
+            $original = $num;
+            $convertido = "";
 
+            while ($num > 0) 
+            {
+                $resto = $num % $base;             
+                $convertido = $resto . $convertido;
+                $num = ($num - $resto) / $base;
+            }
+
+            if ($original == 0) 
+            {
+                $convertido = "0";
+            }
+
+            echo "Número $original en base $base = $convertido<br>";
+        ?>
+    </body>
+</html>
