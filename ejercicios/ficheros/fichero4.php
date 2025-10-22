2522577c30df31e5c86e19993a645980e242c67f
Qixian Guan <151907247+nanbeiyyds@users.noreply.github.com>
2025-10-22 10:37:39 +0200

    Add files via upload

---
+++ ejercicios/ficheros/fichero4.php
@@ -0,0 +1,24 @@
+<?php
+/* programa php que muestre por pantalla el contenido del fichero alumnos2.txt 
+como una tabla HTML. Se mostrará, después de los datos de los alumnos, un mensaje con el número de 
+filas que se han leído del fichero. */
+ function test_imput($data)
+{
+    $data = trim($data);
+    $data = stripcslashes($data);
+    $data = htmlspecialchars($data)
+    return $data;
+}
+$archivo = "alumnos2.txt";
+$lineas = file($archivo);
+echo "<table border='1'>";
+echo "<tr><th>Nombre</th><th>Apellido1</th><th>Apellido2</th><th>Fecha Nac.</th><th>Localidad</th></tr>";
+foreach ($lineas as $linea) {
+    $campos = explode("##", trim($linea));
+    echo "<tr>";
+    foreach ($campos as $c) echo "<td>$c</td>";
+    echo "</tr>";
+}
+echo "</table>";
+echo "<p>Total alumnos: ".count($lineas)."</p>";
+?>
